9fcd9c656dbb7ecabd02ebff7b211a9be3123f01
Lucaspaula6 <lucas.paula@rethink.dev>
2022-05-04 10:28:53 -0300

    Tudo finalizado
    Há pendências, como: qual a origem dos branches e commits do projeto? E das stars do dev?
    Além disso MUITAS melhorias podem ser feitas no CSS.
    
    Creio atender às demandas propostas.

--- public/js/projectShow.js
+++ public/js/projectShow.js
@@ -1,6 +1,6 @@
 const login = new URLSearchParams(window.location.search).get("login");
 // console.log(login);
-const name = new URLSearchParams(window.location.search).get("name");
+const projectName = new URLSearchParams(window.location.search).get("name");
 // console.log(name);
 const userName = "lucaspaula6";
 const token = "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";
@@ -9,10 +9,23 @@ const projectName_div = document.getElementById("projectName");
 const overview_div = document.getElementById("overview");
 const projectDescriprion_div = document.getElementById("projectDescriprion");
 
+const totalCommits = document.getElementById("totalCommits");
+const totalBranches = document.getElementById("totalBranches");
+const totalWatches = document.getElementById("totalWatches");
+const totalForks = document.getElementById("totalForks");
+const totalStars = document.getElementById("totalStars");
+
+const profilePic = document.getElementById("profilePic");
+const indivName = document.getElementById("indivName");
+const indivJob = document.getElementById("indivJob");
+const indivProjects = document.getElementById("indivProjects");
+const indivStars = document.getElementById("indivStars");
+const indivFollowers = document.getElementById("indivFollowers");
+
 const renderProject = async () => {
-  const urlProject = `https://api.github.com/repos/${login}/${name}`;
+  const urlProject = `https://api.github.com/repos/${login}/${projectName}`;
   const urlColors = `http://localhost:3000/color`;
-  const urlLang = `https://api.github.com/repos/${login}/${name}/languages`;
+  const urlLang = `https://api.github.com/repos/${login}/${projectName}/languages`;
   const urlUser = `https://api.github.com/users/${login}`;
 
   const project = await fetch(urlProject, {
@@ -27,7 +40,25 @@ const renderProject = async () => {
 
   projectName_div.innerHTML = project.name;
   console.log(project);
-  projectDescriprion_div.innerHTML = project.description;
+
+  let projectInfo = {
+    stars: project.stargazers_count,
+    forks: project.forks,
+    watchers: project.watchers_count,
+    branches: 5,
+    // https://api.github.com/repos/filiperethink/certificates-rethink-academy/branches
+    commits: 5,
+    // https://api.github.com/repos/filiperethink/certificates-rethink-academy/commits
+    description: project.description,
+  };
+
+  totalCommits.innerHTML = projectInfo.commits;
+  totalBranches.innerHTML = projectInfo.branches;
+  totalWatches.innerHTML = projectInfo.watchers;
+  totalForks.innerHTML = projectInfo.forks;
+  totalStars.innerHTML = projectInfo.stars;
+
+  projectDescriprion_div.innerHTML = projectInfo.description;
 
   const arr = Object.keys(languages);
   //   console.log(arr);
@@ -40,14 +71,8 @@ const renderProject = async () => {
   const user = await fetch(urlUser, {
     headers: { Authorization: "Basic" + btoa(`${userName}:${token}`) },
   }).then((res) => res.json());
-  const profilePic = document.getElementById("profilePic");
-  const indivName = document.getElementById("indivName");
-  const indivJob = document.getElementById("indivJob");
-  const indivProjects = document.getElementById("indivProjects");
-  const indivStars = document.getElementById("indivStars");
-  const indivFollowers = document.getElementById("indivFollowers");
 
-  let info = {
+  let userInfo = {
     login: user.login,
     name: user.name == null ? user : user.name,
     company: user.company == null ? "Rethink" : user.company,
@@ -56,12 +81,12 @@ const renderProject = async () => {
     avatar: user.avatar_url,
   };
 
-  profilePic.src = info.avatar;
-  indivName.innerHTML = info.name;
-  indivJob.innerHTML = info.company;
-  indivProjects.innerHTML = info.repos;
+  profilePic.src = userInfo.avatar;
+  indivName.innerHTML = userInfo.name;
+  indivJob.innerHTML = userInfo.company;
+  indivProjects.innerHTML = userInfo.repos;
   indivStars.innerHTML = "?";
-  indivFollowers.innerHTML = info.followers;
+  indivFollowers.innerHTML = userInfo.followers;
 };
 
 // `https://api.github.com/repos/${login}/${name}`
